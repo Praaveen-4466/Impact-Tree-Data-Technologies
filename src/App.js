08e38e5428bbb16f6806894e9ea90548fbbd97a9
Praveen <gsaipraveen4466@gmail.com>
2025-06-25 17:41:16 +0530

    Inital commit with source code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from 'react';
+
+function EmailForm() {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Email submitted: ${email}`);
+    setEmail('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "body-style">
+      <h1 className = "heading">Email Form</h1>
+      <form className = "form-style" onSubmit={handleSubmit}>
+        <label className='label'>Email:
+          <input type = "email" className="input-email" value = {email} onChange={(event) => setEmail(event.target.value)} required />
+        </label>
+        <button className = "button-submit" type="submit">Submit</button>
+      </form>
     </div>
   );
 }
 
-export default App;
+export default EmailForm;
